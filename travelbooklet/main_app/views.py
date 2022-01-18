129ec1357b5d521013e48de684e735b1c57a5f5d
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-01-17 21:04:55 -0800

    Access recommended locations data on home page

--- travelbooklet/main_app/views.py
+++ travelbooklet/main_app/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 import json
 import os
 
-# Amadeus API
+# Amadeus API keys
 client_id = os.getenv('AMADEUS_API_KEY')
 client_secret = os.getenv('AMAEDEUS_API_SECRET')
 
@@ -17,42 +17,38 @@ amadeus = Client(
     client_secret = client_secret
 )
 
-# Get access token
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-}
-
-data = {
-  'grant_type': 'client_credentials',
-  'client_id': client_id,
-  'client_secret': client_secret
-}
-
-response = requests.post('https://test.api.amadeus.com/v1/security/oauth2/token', headers=headers, data=data)
-
-print(response.json())
-token = response.json()
-print(token['access_token'])
-
-# Call API
-headers = {
-    'Authorization': f'Bearer {token["access_token"]}',
-}
-
-params = (
-    ('origin', 'PAR'),
-    ('maxPrice', '200'),
-)
 
 
-response = requests.get('https://test.api.amadeus.com/v1/shopping/flight-destinations', headers=headers, params=params)
 
-print(response.json())
+
+
 
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    # Get Amadeus API access token
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    data = {
+    'grant_type': 'client_credentials',
+    'client_id': client_id,
+    'client_secret': client_secret
+    }
+    response = requests.post('https://test.api.amadeus.com/v1/security/oauth2/token', headers=headers, data=data)
+    token = response.json()
+    # Call API - travel recommendation
+    headers = {
+        'Authorization': f'Bearer {token["access_token"]}',
+    }
+    params = (
+        ('cityCodes', 'SFO'),
+        ('travelerCountryCode', 'US'),
+    )
+    response = requests.get('https://test.api.amadeus.com/v1//reference-data/recommended-locations', headers=headers, params=params)
+    data= response.json()
+    # Render home page
+    return render(request,'home.html',{'data':data})
 
 def signup(request):
     error_message = ''
