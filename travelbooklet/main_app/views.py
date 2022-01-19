d7873532b2a241c9d39af196349828b176173521
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-01-19 01:39:32 -0800

    Use Teleport API to generate city information and image

--- travelbooklet/main_app/views.py
+++ travelbooklet/main_app/views.py
@@ -7,7 +7,8 @@ import requests
 from django.contrib import messages
 import json
 import os
-import random
+import random #random.choices()
+import re # regex
 
 # Amadeus API keys
 client_id = os.getenv('AMADEUS_API_KEY')
@@ -20,57 +21,58 @@ amadeus = Client(
 
 # Create your views here.
 def home(request):
-    iata_array = [
-        # France
-        'CDG','NCE',
-        # Spain
-        'BCN',
-        # Italy
-        'FCO','VCE',
-        # Germany
-        'FRA','TXL',
-        # United Kingdom
-        'LHR','LGW',
-        # America
-        'ATL','LAX','JFK','SFO','SEA','LAS','MIA','HNL',
-        # Mexico
-        'MEX','CUN','TIJ',
-        # Saudi Arabia
-        'JED','RUH',
-        # Netherlands
-        'AMS','EIN',
-    ]
-    # Get Amadeus API access token
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
-    data = {
-    'grant_type': 'client_credentials',
-    'client_id': client_id,
-    'client_secret': client_secret
-    }
-    response = requests.post('https://test.api.amadeus.com/v1/security/oauth2/token', headers=headers, data=data)
-    token = response.json()
-    # Call API - travel recommendation
-    headers = {
-        'Authorization': f'Bearer {token["access_token"]}',
-    }
-    params = (
-        ('cityCodes', f'{random.choice(iata_array)}'),
-    )
-    response = requests.get('https://test.api.amadeus.com/v1//reference-data/recommended-locations', headers=headers, params=params)
-    data= response.json()
-    print('featured city',data["data"][0]["name"])
-    # Imsea Image search API - 3 relevant destinations
-    image1 = requests.get(f'https://imsea.herokuapp.com/api/1?q={data["data"][0]["name"]}').json()
-    image2 = requests.get(f'https://imsea.herokuapp.com/api/1?q={data["data"][1]["name"]}').json()
-    image3 = requests.get(f'https://imsea.herokuapp.com/api/1?q={data["data"][2]["name"]}').json()
+
+
+    # # ------------------ Amadeus API --------------------
+    # # Get Amadeus API access token
+    # headers = {
+    #     'Content-Type': 'application/x-www-form-urlencoded',
+    # }
+    # data = {
+    # 'grant_type': 'client_credentials',
+    # 'client_id': client_id,
+    # 'client_secret': client_secret
+    # }
+    # response = requests.post('https://test.api.amadeus.com/v1/security/oauth2/token', headers=headers, data=data)
+    # token = response.json()
+    # # Call API - travel recommendation
+    # headers = {
+    #     'Authorization': f'Bearer {token["access_token"]}',
+    # }
+    # params = (
+    #     ('cityCodes', f'{random.choice(iata_array)}'),
+    # )
+    # response = requests.get('https://test.api.amadeus.com/v1//reference-data/recommended-locations', headers=headers, params=params)
+    # data= response.json()
+    # print('featured city',data["data"][0]["name"])
+
+
+
+    # -------------- Teleport API --------------
+    # Access list of urban areas
+    urban_areas = requests.get('https://api.teleport.org/api/urban_areas/').json()['_links']['ua:item']
+
+    # Choose 3 random ones from list and add to array
+    random_area_list = random.choices(urban_areas, k=3)
+    data_array = []
+
+    # Call Teleport API and fetch info
+    for idx, city in enumerate(random_area_list):
+        city_info = requests.get(f'https://api.teleport.org/api/cities/?search={city["name"]}&embed=city%3Asearch-results%2Fcity%3Aitem%2Fcity%3Aurban_area%2Fua%3Ascores').json()
+        
+        city_scores = (city_info['_embedded']['city:search-results'][0]['_embedded']['city:item']['_embedded']['city:urban_area']['_embedded']['ua:scores']['categories']) 
+        city_summary = (city_info['_embedded']['city:search-results'][0]['_embedded']['city:item']['_embedded']['city:urban_area']['_embedded']['ua:scores']['summary'])
+        city_country = (city_info['_embedded']['city:search-results'][0]['_embedded']['city:item']['_embedded']['city:urban_area']['_links']['ua:countries'][0]['name'])
+        
+        city_image_links = (city_info['_embedded']['city:search-results'][0]['_embedded']['city:item']['_embedded']['city:urban_area']['_links']['ua:images']['href'])
+        city_image = requests.get(city_image_links).json()['photos'][0]['image']
+
+        city_full_name= (city_info['_embedded']['city:search-results'][0]['_embedded']['city:item']['_embedded']['city:urban_area']['full_name'])
+        data_array.extend([[city_full_name, city_country, city_image, city_scores, city_summary]])
+    
     # Render home page
     return render(request,'home.html',{
-        'data':data,
-        'image1':image1,
-        'image2':image2,
-        'image3':image3
+        'data': data_array
         })
 
 def signup(request):
